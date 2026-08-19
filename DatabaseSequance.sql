e28e1075a40846b63d96a10cb9fe520c1395a18c
KiranKumar00018 <balikirankumar75@gmail.com>
2026-08-19 19:57:18 +0530

    Database sequrnces

---
+++ DatabaseSequance.sql
@@ -0,0 +1,37 @@
+
+---- sequence for department_id ----
+create sequence sq_department_id
+start with 10
+increment by 10;
+---- sequence for student_id ----
+create sequence sq_student_id
+start with 1
+increment by 1;
+---- sequence for course_id ----
+create sequence sq_course_id
+start with 1
+increment by 1;
+---- sequence for subject_id ----
+create sequence sq_subject_id
+start with 1
+increment by 1;
+---- sequence for enrollment_id ----
+create sequence sq_enrollment_id
+start with 1
+increment by 1;
+---- sequence for mark_id ----
+create sequence sq_mark_id
+start with 1
+increment by 1;
+---- sequence for user_id ----
+create sequence sq_user_id
+start with 1
+increment by 1;
+---- sequence for audit_id ----
+create sequence sq_audit_id
+start with 1
+increment by 1;
+---- sequence for error_id ----
+create sequence sq_error_id
+start with 1
+increment by 1;
